f9c77e69a916572951ae39e914a3a6af74369f11
shriram24P <patil.shriram98@gmail.com>
2023-02-06 12:45:28 +0530

    adding footer

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import HeroSection from './components/HeroSection';
 import Services from './components/Services';
 import Trusted from './components/Trusted';
@@ -13,6 +14,7 @@ const Home = () => {
     <HeroSection myData={data} />
     <Services />
     <Trusted/>
+    
     </>
   
 };
